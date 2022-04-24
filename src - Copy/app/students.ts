06af2487de1d909dcdee67b70b1b7fdc225bb073
Melvin Josue Castillo <32580232+MelvinCastillo@users.noreply.github.com>
2022-04-23 23:29:37 -0400

    Delete src - Copy directory

--- src - Copy/app/students.ts
+++
@@ -1,9 +0,0 @@
-export class students {
-    secuencia : number = 0
-    studentId : string = ""
-    name : string = ""
-    rollNumber : string = ""
-    professorId : string = ""
-    fees : number = 0
-}
-
--- src - Copy/app/users.ts
+++
@@ -1,8 +0,0 @@
-export class Users {
-        success	: boolean = true
-    usuarioid	: string = ""
-    nombre		: string = ""
-    apellido	: string = ""
-    tipo		: string = ""
-    email		: string = ""
-}
